6fd391b4021241d3d63abc5b1698b0316a5786b2
Victoria <vikiber2013@gmail.com>
2021-01-20 17:53:05 +0200

    Heroku

--- test_form/settings.py
+++ test_form/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'gxdc-bs3$+mt$o%slx&#gwnapibm79u))@cy^$by(1jddqmig&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stormy-mountain-84583.herokuapp.com']
 
 
 # Application definition
